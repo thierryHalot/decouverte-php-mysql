dbbbb27e314a162575531393788661c99030f2d5
Thierry <thierry@beweb-16.176R>
2018-04-24 19:15:43 +0200

    test de calcul simple en php

---
+++ openClassroom/test/calculeEnPhP.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thierry
+ * Date: 24/04/18
+ * Time: 18:18
+ */
+/** var_dump(nombre); permet de determiner  */
+$nombre = 2 + 4;
+
+echo '2 + 4 = '. $nombre .' en Php <br/>'; // nombre vaut 6
+
+$nombre = 5 - 1;
+
+echo '5 - 1 = '. $nombre .' en Php<br/>'; // nombre vaut 4
+
+$nombre = 3 * 5;
+
+echo '3 * 5 = '. $nombre . ' en Php<br/>';// nombre vaut 15
+
+$nombre = 10 / 2;
+
+echo '10 / 2 = '. $nombre . ' en Php<br />';// nombre vaut 5
+
+$resultat;// declaration d'une variable en php
+
+$adition = 1 + 2; // adition vaut 3
+$multiplication = 5 *3; //multiplication vaut 15
+
+$resultat = $adition + $multiplication;
+
+echo '3 + 15 = '. $multiplication . ' en Php <br/>';
