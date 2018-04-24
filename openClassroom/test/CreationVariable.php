62cbf435faa2e3b20f34fe530f036f00e2cdfc55
Thierry <thierry@beweb-16.176R>
2018-04-24 18:05:18 +0200

    test des variables : declaration et concatenation

---
+++ openClassroom/test/CreationVariable.php
@@ -0,0 +1,21 @@
+
+<?php
+  // création d'une variable en php, on uttilise toujour le $ suivie du nom de la variable
+
+    $age_du_visiteur = 27; // variable de type int
+    $boolean = true; // type boolean
+    $float = 25.3; // type float
+    $texte = 'hello "World"';
+    $sans_valeur = NUll; //variable qui ne contient rien
+    $concatenation = 'voila';
+
+
+
+    // la commande echo permet d'afficher du texte mais aussi, d'afficher le contenu d'une variable dans le navigateur
+    echo 'pour concatener des chaines, on fait comme sa : ' . $concatenation . ' sa marche !!! <br />'; //on peut également inserer des balise
+    echo $age_du_visiteur . '<br />';
+    echo $texte . '<br />';
+
+
+
+
